30c76e8c3abbe0682383461ee763de861d85cb0d
sandalal <53229680+sandalal@users.noreply.github.com>
2019-07-23 22:32:53 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { Book } from '../../app/book';
+
+export const MYBOOKS: Book[] = [
+    {name: 'The Alchemist', author: 'Paulo Ceolho' , genre: 'Adventure', year: '1993', pic: 'assets/images/alchemist.jpg' },
+    {name: 'The Four Agreements', author: 'Don Miguel Ruiz', genre: 'Personal Growth' , year: '1997', pic: 'assets/images/fourAgreements.jpg'},
+    {name: 'Harry Potter and the Order of the Pheonix', author: 'J. K. Rowling', genre: 'Fantasy' , year: '2003', pic: 'assets/images/pheonix.jpg'},
+    {name: '1984', author: 'George Orwell', genre: 'Dystopian' , year: '1949', pic: 'assets/images/1984.jpg'}
+]
